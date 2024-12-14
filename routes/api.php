b70b8ca933b0e5a599946c8c94dae6b314692f23
porozera <aufarnugratamaps@gmail.com>
2024-12-15 01:31:33 +0700

    Transaction & User done

--- routes/api.php
+++ routes/api.php
@@ -2,10 +2,12 @@
 
 use App\Http\Controllers\CategoriesController;
 use App\Http\Controllers\TransactionsController;
+use App\Http\Controllers\UsersController;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('categories', CategoriesController::class);
 Route::apiResource('transactions', TransactionsController::class);
-    
+Route::apiResource('users', UsersController::class);
+
 
 
