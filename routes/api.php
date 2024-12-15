8c4f4d192072572d27c979c52a529e47e4851ba6
antoniosirait29 <antoniosirait29@gmail.com>
2024-12-15 13:53:00 +0700

    Fixing double import

--- routes/api.php
+++ routes/api.php
@@ -1,29 +1,25 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CategoriesController;
 use App\Http\Controllers\TransactionsController;
 use App\Http\Controllers\UsersController;
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BudgetsController;
-use App\Http\Controllers\CategoriesController;
 use App\Http\Controllers\SavingGoalsController;
-use App\Http\Controllers\TransactionsController;
 use App\Http\Controllers\NotificationsController;
 
 Route::apiResource('categories', CategoriesController::class);
 Route::apiResource('transactions', TransactionsController::class);
 Route::apiResource('users', UsersController::class);
-
-
-    Route::apiResource('categories', CategoriesController::class);
-    Route::apiResource('saving-goals', SavingGoalsController::class);
-    Route::apiResource('notifications', NotificationsController::class);
-    Route::apiResource('budgets', BudgetsController::class);
+Route::apiResource('categories', CategoriesController::class);
+Route::apiResource('saving-goals', SavingGoalsController::class);
+Route::apiResource('notifications', NotificationsController::class);
+Route::apiResource('budgets', BudgetsController::class);
 
 
 
-    //notification alrd readed
-    Route::patch('notifications/{id}/read', [NotificationsController::class, 'markAsRead']);
+//notification alrd readed
+Route::patch('notifications/{id}/read', [NotificationsController::class, 'markAsRead']);
 
 
 
